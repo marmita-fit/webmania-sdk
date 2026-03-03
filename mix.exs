f3a3f1ecd85ae39cfdcdc809b3056f2245af94ab
Bruno Casali <brunoocasali@gmail.com>
2026-03-02 23:33:59 -0300

    Update version to v0.3.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebmaniaNfe.MixProject do
   def project do
     [
       app: :webmania_nfe,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
